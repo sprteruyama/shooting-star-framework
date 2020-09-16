2d46b4dd5494f7dcbedec016b45aaec878ed3142
sprteruyama <teruyama@springboard-inc.jp>
2020-09-16 15:43:13 +0900

    added a function to Controller->outJson for output response code

--- src/Classes/Controller.php
+++ src/Classes/Controller.php
@@ -170,8 +170,9 @@ class Controller extends Base
         $this->view->render($template, $layout);
     }
 
-    public function outJson($data = [])
+    public function outJson($data = [], $statusCode = 200)
     {
+        http_response_code($statusCode);
         header('Content-Type: application/json');
         echo json_encode($data);
         $this->isRendered = true;
